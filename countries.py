76557127fa737c3658ba8de6500fa294e90f597f
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-09-24 17:11:46 +0500

    countries with their capitals

---
+++ countries.py
@@ -0,0 +1,18 @@
+# Create a dictionary where the keys are the names of 5 different 
+# countries and the values are their capitals. Write a program to display
+# all the countries and their capitals.
+
+
+
+# Create dictionary of 5 different countries with values are their capitals:
+countries = {
+    "france": "paris",
+    "japan": "tokye",
+    "canada": "ottawa",
+    "india": "New delhi",
+    "italy": "rome"
+}
+
+
+# Display all the countries and their capitals:
+print(countries)
